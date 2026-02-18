02f19cb953a2c299a7f39f531ef0a91ff08fd651
Louis Levacher <louislevacher76@gmail.com>
2026-02-18 11:25:29 +0100

    Ajout de flag pour les clés

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "api-fizzbuzz/api"
+import (
+	"api-fizzbuzz/api"
+
+	"github.com/spf13/pflag"
+)
 
 func main() {
-	cles := map[string]struct{}{"testcle":struct{}{}}
+	flagCles := pflag.StringSliceP("key", "k", []string{}, "Clé d'API")
+
+	pflag.Parse()
+
+	cles := map[string]struct{}{}
+	for _, cle := range *flagCles {
+		cles[cle] = struct{}{}
+	}
+
 	r := api.SetupAPI(cles)
 	r.Run(":8080")
 }
